5a51306cd4162558b40a2d2e6dcb3eab4eccef8e
Isaac Salas <iscprepa@gmail.com>
2024-03-09 20:32:51 -0600

    Deleting everything

--- UI/Credits/credits.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Label/AnimationPlayer.play("Credits")
-
-
-func _on_texture_button_2_pressed():
-	get_tree().change_scene_to_file("res://Menu/Title-Screen.tscn")
