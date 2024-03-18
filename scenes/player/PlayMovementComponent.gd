9e4ebcfc47610733e05eaf41b9977338701bc26f
Isaac Salas <iscprepa@gmail.com>
2024-03-17 19:39:24 -0600

    Added Movement Component for playable characters

---
+++ scenes/player/PlayMovementComponent.gd
@@ -0,0 +1,22 @@
+class_name PlayMovementComponent
+extends Node2D
+
+@export var Target : CharacterBody2D
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta):
+
+	var directionX = Input.get_axis("ui_left", "ui_right")
+	var directionY = Input.get_axis("ui_up", "ui_down")
+	if directionX:
+		Target.velocity.x = directionX * SPEED
+	else:
+		Target.velocity.x = move_toward(Target.velocity.x, 0, SPEED)
+		
+	if directionY:
+		Target.velocity.y = directionY * SPEED
+	else:
+		Target.velocity.y = move_toward(Target.velocity.y, 0, SPEED)
+		
+	Target.move_and_slide()
---
+++ scenes/player/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+@onready var movement_component = $PlayMovementComponent
+
+
+func _ready():
+	pass
+
+
+
