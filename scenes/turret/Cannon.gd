189db461be566daf57fb2ecad48e13de7c311707
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2024-03-29 21:30:26 -0600

    Added simple button interaction and layout, no hardcore implementations yet.
    lesgo

--- scenes/turret/Cannon.gd
+++ scenes/turret/Cannon.gd
@@ -1,11 +1,15 @@
 extends StaticBody2D
-
-
+var coords: Vector2i  = Vector2i(0,0)
 
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+func _unhandled_input(event):
+	if Input.is_action_pressed("right_click") and $"../Ground".local_to_map(get_global_mouse_position()) == coords:
+		queue_free()
+		
