e6a9c08dbff2aee932f549dbb3d5f14a099442cb
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2024-03-31 01:08:00 -0600

    Polished placing towers and showing consequences of the path after modifying the map

--- scenes/UserInterface/UI.gd
+++ scenes/UserInterface/UI.gd
@@ -14,22 +14,19 @@ func _on_turret_1_pressed():
 	var turret1 = "MG"
 	emit_signal("turret_selected", turret1)
 
-
 func _on_turret_2_pressed():
 	var turret2 = "missile_launcher"
 	emit_signal("turret_selected", turret2)
-
 
 func _on_turret_3_pressed():
 	var turret3 = "cannon"
 	emit_signal("turret_selected", turret3)
 
-
 func _on_turret_4_pressed():
 	var turret4 = "empty"
 	emit_signal("turret_selected", turret4)
-	
-func _unhandled_key_input(event):
+
+func _unhandled_key_input(_event):
 	if Input.is_action_just_pressed("1"):
 		_on_turret_1_pressed()
 	elif Input.is_action_just_pressed("2"):
@@ -38,4 +35,3 @@ func _unhandled_key_input(event):
 		_on_turret_3_pressed()
 	elif Input.is_action_just_pressed("4"):
 		_on_turret_4_pressed()
-	
