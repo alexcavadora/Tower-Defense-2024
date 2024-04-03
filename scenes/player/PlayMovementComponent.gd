791126ad6d960e57da8a19b83cb5e723b6b793db
Isaac Salas <iscprepa@gmail.com>
2024-04-02 19:48:21 -0600

    Testing

--- scenes/player/PlayMovementComponent.gd
+++ scenes/player/PlayMovementComponent.gd
@@ -1,6 +1,6 @@
 class_name PlayMovementComponent
 extends Node2D
-
+@onready var player_animation = $"../PlayerAnimation"
 @export var Target : CharacterBody2D
 @export var SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
@@ -10,13 +10,16 @@ func _physics_process(delta):
 	var directionX = Input.get_axis("ui_left", "ui_right")
 	var directionY = Input.get_axis("ui_up", "ui_down")
 	if directionX:
+		player_animation.run_animation()
 		Target.velocity.x = directionX * SPEED
 	else:
+		player_animation.idle_animation()
 		Target.velocity.x = move_toward(Target.velocity.x, 0, SPEED)
 		
 	if directionY:
 		Target.velocity.y = directionY * SPEED
 	else:
+		player_animation.idle_animation()
 		Target.velocity.y = move_toward(Target.velocity.y, 0, SPEED)
 		
 	Target.move_and_slide()
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 @onready var movement_component = $PlayMovementComponent
-
+@onready var player_animation = $PlayerAnimation
 
 func _ready():
+	player_animation.start_animation()
 	pass
 
 
