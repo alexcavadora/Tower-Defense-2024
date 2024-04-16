1f75839356164bb368f6d6bf8d4a98023291e05b
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2024-04-15 23:22:56 -0600

    cannon rotation added and fixed

--- scenes/turret/Cannon.gd
+++ scenes/turret/Cannon.gd
@@ -1,17 +1,29 @@
 extends StaticBody2D
 var coords: Vector2i  = Vector2i(0,0)
 var ghost = false
+var enemies = []
+var target
+var rotation_speed = 0.05
 
-func _ready():
-
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
-
+	if ghost:
+		return
+	if enemies != [] and target != null:
+		$AnimatedSprite2D.rotation = lerp_angle($AnimatedSprite2D.rotation, get_angle_to(target.global_position) + PI/2 , rotation_speed)
+	else:
+		$AnimatedSprite2D.rotation = lerp_angle($AnimatedSprite2D.rotation, 0.0, rotation_speed)
 
 func _unhandled_input(_event):
 	if Input.is_action_pressed("right_click") and !ghost and $"../Ground".local_to_map(get_global_mouse_position()) == coords:
 		queue_free()
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group('enemy'):
+		enemies.append(body)
+		target = enemies[0]
+
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group('enemy'):
+		target = enemies[0]
+		enemies.erase(body)
