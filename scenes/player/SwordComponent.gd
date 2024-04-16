81a1dc92f9af2b871b5afc6699e3d5963cb1a2bf
Isaac Salas <iscprepa@gmail.com>
2024-04-16 17:18:10 -0600

    Sprite flip and sword flip

--- scenes/player/SwordComponent.gd
+++ scenes/player/SwordComponent.gd
@@ -2,13 +2,6 @@ extends AnimatedSprite2D
 class_name SwordComponent
 @onready var MousePos
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	MousePos = get_local_mouse_position()
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+@export var Sprite : AnimatedSprite2D
+@export var Sword : AnimatedSprite2D
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("ui_right"):
+		Sprite.flip_h = false
+		Sword.position.x = 9
+	elif Input.is_action_just_pressed("ui_left"):
+		Sprite.flip_h = true
+		Sword.position.x = -9
+		
